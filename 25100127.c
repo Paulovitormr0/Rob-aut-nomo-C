c5f7c749e501603e61f935c41a226446de5f6a29
Paulovitormr0 <paulo.dev.ucb>
2026-05-19 18:00:27 -0300

    Alterações no InicializarCaminho

--- 25100127.c
+++ 25100127.c
@@ -21,7 +21,32 @@ Caminho * InicializarCaminho(const char *Sequencia, int xInicial, int yInicial){
     Rota->N = 0;
     int yAux = yInicial;
     int xAux = xInicial;
-    
+    Posicao *ultimaPos = inicio; //guardar sempre o final da lista
+    Comando *ultimoCmd = NULL; //guardar sempre o final dos comandos
+    for(int i = 0; Sequencia[i] != '\0'; i++){
+        switch (Sequencia[i])
+        {
+        case 'N':
+            yAux++;
+            break;
+        case 'S':
+            yAux--;
+            break;
+        case 'L':
+            xAux++;
+            break;
+        case 'O':
+            xAux--;
+            break;
+        default:
+            printf("Caractere invalido");//Deixar mais bonitinha essa msg
+
+            break;
+        }//switch case para mover o robo
+            Comando *novoCmd = malloc(sizeof(*novoCmd));
+            novoCmd->Direcao = Sequencia[i];
+            novoCmd->Proximo = NULL;
+    } 
 
 }
 
